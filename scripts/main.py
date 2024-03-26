19dcc3062006df7be51dcd221639ce65cb94d2a8
James Buchanan <buchananja.github@pm.me>
2024-03-26 14:28:13 +0000

    Added functionality to convert raw data into csv format.

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,8 @@
+import re
+import time
 import httpx
+import pandas as pd
+from io import StringIO
 from bs4 import BeautifulSoup
 
 
@@ -12,13 +16,40 @@ page_response = httpx.get(page_url)
 # creating a soup object by parsing requested html
 soup = BeautifulSoup(page_response, 'html.parser')
 
-# extracts all 'tr' (table rows), finds 'td' (table data), gets first 'td' 
-# (station name); gets 'a' (anchor) tags and extracts href (contains url),
-# gets text data from url and updates dict with station name key and data value
+# finds table rows and data, gets first column (name), gets a tags and extracts 
+# href url, gets data from url, updates dict with station name and data
+def clean_data(response):
+    '''cleans each weather station data'''
+
+    # removes characters preceding 'yyyy'
+    data = response.text.lower()
+    index = data.index('yyyy')
+    data = data[index:]
+
+    # removes "provisional" substring
+    data = data.replace(r'provisional', '')
+
+    # replaces all consecutive whitespace except returns with single comma
+    data_lines = data.splitlines()
+    data_lines = [re.sub(r'[^\S\r\n]+', ',', line) for line in data_lines]
+
+    # removes leading/trailing comma from lines where present
+    data_lines = [line[1:] if line.startswith(',') else line for line in data_lines]
+    # debugging trailing commas
+    [print(f'\nLine: {repr(line[-10:-1])}') for line in data_lines]
+    # data_lines = [line.replace('\r\n', '\n').replace(',\n', '\n') for line in data_lines]
+    # data_lines = [line[:-2] + '\n' if line.startswith(',\n') else line for line in data_lines]
+
+    # removes second line
+    data = '\n'.join(data_lines[0:1] + data_lines[2:])
+
+    return data
+
+
 table_rows = soup.find_all('tr')
 station_dict = dict()
 
-for row in table_rows:
+for index, row in enumerate(table_rows):
     # gets station name from table data
     column_0 = row.find('td')
     if column_0:
@@ -27,6 +58,13 @@ for row in table_rows:
     a_tags = row.find_all('a')
     for tag in a_tags:
         print(f'Requesting data from {station_name.title()}...')
+        time.sleep(0.1)
         station_url = tag.get('href')
         station_response = httpx.get(station_url)
-        station_dict[station_name] = station_response.text
+        station_dict[station_name] = clean_data(station_response)
+    
+    # writes aberporth data
+    if index == 1:
+        with open(f'{station_name}.txt', 'w') as file:
+            file.write(station_dict[station_name])
+        break
