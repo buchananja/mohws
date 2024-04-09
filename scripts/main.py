29bc47b3d439586c93eec1c1f31fd9ac8d9871bd
James Buchanan <buchananja.github@pm.me>
2024-04-09 22:28:02 +0100

    Added additional cleaning functions.

--- scripts/main.py
+++ scripts/main.py
@@ -4,6 +4,32 @@ import httpx
 from bs4 import BeautifulSoup
 
 
+def remove_trailing_char(line, trailing_char):
+    '''returns line with trailing character removed'''
+
+    if line.startswith(trailing_char):
+        line = line[:-1]
+
+    return line
+
+
+def remove_leading_char(line, leading_char):
+    '''returns line with leading character removed'''
+
+    if line.startswith(leading_char):
+        line = line[1:]
+
+    return line
+
+
+def replace_consecutive_spaces(line, replacement_char):
+    '''returns line with all consecutive whitespace replaced with character'''
+    
+    line = re.sub(r'[^\S\r\n]+', replacement_char, line)
+
+    return line
+
+
 def get_index_text(phrase, split_char, index):
     '''gets text from index in phrase split by character'''
 
@@ -90,10 +116,10 @@ def get_braemar_geography(geography_lines):
     amsl_2 = amsl_2_phrase[0:3]
 
     # extracts year ranges from raw year phrases
-    first_year_phrase = get_text_between_indexes(amsl_1_phrase, '(', ')')
-    first_year_range = get_text_numerics(first_year_phrase, ' ', 0, -1)
-    second_year_phrase = get_text_between_indexes(amsl_2_phrase, '(', ')')
-    second_year = get_text_numerics(second_year_phrase, ' ', 1)[0]
+    # first_year_phrase = get_text_between_indexes(amsl_1_phrase, '(', ')')
+    # first_year_range = get_text_numerics(first_year_phrase, ' ', 0, -1)
+    change_year_phrase = get_text_between_indexes(amsl_2_phrase, '(', ')')
+    change_year = get_text_numerics(change_year_phrase, ' ', 1)[0]
 
     # extracts longitude/latitude for second year range
     lat_long_phrase = get_index_text(geography_lines, ',', 3).strip().split(' ')
@@ -102,15 +128,11 @@ def get_braemar_geography(geography_lines):
 
     # packs geography details into dict
     geography_dict = dict()
-    geography_dict['first_year'] = first_year_range
-    geography_dict['second_year'] = second_year
+    geography_dict['change_year'] = change_year
     geography_dict['amsl_1'] = amsl_1
     geography_dict['amsl_2'] = amsl_2
     geography_dict['longitude'] = longitude
     geography_dict['latitude'] = latitude
-
-    for key, value in geography_dict.items():
-        print(key, value)
 
     return geography_dict
 
@@ -130,9 +152,6 @@ def get_lowestoft_geography(geography_lines):
     geography_dict['longitude'] = first_location_phrase[17].strip(',')
     geography_dict['latitude'] = first_location_phrase[15]
 
-    for key, value in geography_dict.items():
-        print(key, value)
-
     return geography_dict
 
 
@@ -151,9 +170,6 @@ def get_nairn_geography(geography_lines):
     geography_dict['amsl_2'] = get_string_numerics(first_location_phrase[16])
     geography_dict['longitude'] = first_location_phrase[15]
     geography_dict['latitude'] = first_location_phrase[13]
-
-    for key, value in geography_dict.items():
-            print(key, value)
 
     return geography_dict
 
@@ -182,9 +198,6 @@ def get_southampton_geography(geography_lines):
     geography_dict['longitude'] = first_location_phrase[15]
     geography_dict['latitude'] = first_location_phrase[13]
 
-    for key, value in geography_dict.items():
-            print(key, value)
-
     return geography_dict
 
 
@@ -202,8 +215,8 @@ def get_whitby_geography(geography_lines):
     geography_dict['longitude'] = first_location_phrase[16]
     geography_dict['latitude'] = first_location_phrase[14]
 
-    for key, value in geography_dict.items():
-        print(key, value)
+    # for key, value in geography_dict.items():
+    #     print(key, value)
 
     return geography_dict
 
@@ -221,18 +234,17 @@ def clean_data(response, name):
     geographic_dict = dict()
     if 'amsl' in data_lines[2]:
         geography_lines = ''.join(data_lines[1:3])
-        # print(f'{name}: {geography_lines.split(',')}\n')
 
         if name in 'braemar_no_2':
-            get_braemar_geography(geography_lines)
+            braemar_dict = get_braemar_geography(geography_lines)
         if name in 'lowestoft_monckton_avenue':
-            get_lowestoft_geography(geography_lines)
+            lowesoft_dict = get_lowestoft_geography(geography_lines)
         if name in 'nairn_druim':
-            get_nairn_geography(geography_lines)
+            nairn_dict = get_nairn_geography(geography_lines)
         if name in 'southampton_mayflower_park':
-            get_southampton_geography(geography_lines)
+            southampton_dict = get_southampton_geography(geography_lines)
         if name in 'whitby':
-            get_whitby_geography(geography_lines)
+            whitby_dict = get_whitby_geography(geography_lines)
     else:
         geography_line = data_lines[1]
         location = geography_line.split(', ')[1]
@@ -244,13 +256,10 @@ def clean_data(response, name):
         # inserts longitude/latitude/amsl for station into dict
         geographic_dict[name] = (amsl, longitude, latitude)
 
-    # replaces all consecutive whitespace except returns with single comma
-    data_lines = [re.sub(r'[^\S\r\n]+', ',', line) for line in data_lines]
-
-    # removes leading/trailing comma from lines where present
-    data_lines = [line[1:] if line.startswith(',') else line for line in data_lines]
-    data_lines = [line[:-1] if line.endswith(',') else line for line in data_lines]
-
+    # formats output
+    data_lines = [replace_consecutive_spaces(line, ',') for line in data_lines]
+    data_lines = [remove_leading_char(line, ',') for line in data_lines]
+    data_lines = [remove_trailing_char(line, ',') for line in data_lines]
     data_lines = drop_site_closed_row(data_lines)
     data_lines = drop_header_text(data_lines)
     data_lines = drop_units_row(data_lines)
@@ -288,7 +297,7 @@ def main():
         column_0 = row.find('td')
         if column_0:
             station_name = column_0.text.lower().strip().replace(' ', '_')
-        # requests and stores station data from hyperlink intof dictionary
+        # requests and stores station data from hyperlink into dictionary
         a_tags = row.find_all('a')
         for tag in a_tags:
             # print(f'Requesting data from {station_name.title()}...')
